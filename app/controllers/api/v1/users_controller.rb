92a2b0bbd875d0fe82353341025bd759baff4a4e
lefmarna <panume5ya@gmail.com>
2021-04-16 17:35:25 +0900

    usersコントローラーの作成

---
+++ app/controllers/api/v1/users_controller.rb
@@ -0,0 +1,60 @@
+class Api::V1::UsersController < ApplicationController
+  before_action :authenticate_v1_user!, except: [:index, :show]
+  before_action :set_user, only: [:show, :update, :destroy]
+
+  # GET /users
+  def index
+    # ユーザーの一覧表示に画像も表示したいが、そのための処理はeach文などを使って書かなくてはならないのだろうか？ もっと良い書き方があれば知りたい
+    @users = User.select(:id, :username, :nickname)
+    render json: @users
+  end
+  
+  # GET /users/1
+  def show
+    response = {
+      id: @user.id,
+      username: @user.username,
+      nickname: @user.nickname
+    }
+    if @user.image.attached?
+      response[:image] = url_for(@user.image)
+    end
+    render json: response
+  end
+
+  # POST /users
+  def create
+    @user = User.new(user_params)
+
+    if @user.save
+      render json: @user, status: :created, location: @user
+    else
+      render json: @user.errors, status: :unprocessable_entity
+    end
+  end
+
+  # PATCH/PUT /users/1
+  def update
+    if @user.update(user_params)
+      render json: @user
+    else
+      render json: @user.errors, status: :unprocessable_entity
+    end
+  end
+
+  # DELETE /users/1
+  def destroy
+    @user.destroy
+  end
+
+  private
+    # Use callbacks to share common setup or constraints between actions.
+    def set_user
+      @user = User.find(params[:id])
+    end
+
+    # Only allow a trusted parameter "white list" through.
+    def user_params
+      params.require(:user).permit(:username, :nickname, :image, :email, :password)
+    end
+end
