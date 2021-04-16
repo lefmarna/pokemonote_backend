f6e27ed9b599f4a542fa276ceef89679b57e7f08
lefmarna <panume5ya@gmail.com>
2021-04-16 18:28:58 +0900

    Active Storageの導入

--- app/models/user.rb
+++ app/models/user.rb
@@ -7,6 +7,8 @@ class User < ActiveRecord::Base
          :recoverable, :rememberable, :validatable
   include DeviseTokenAuth::Concerns::User
 
+  has_one_attached :image
+
   with_options presence: true do
     validates :username, uniqueness: {case_sensitive: false}, length: { minimum: 3, maximum: 25 }, format: { with: /\A[a-z\d]+\z/i }
     validates :nickname
