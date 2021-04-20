aa521bdc33d48441305dd5b01dfeb0b259d7ae96
lefmarna <panume5ya@gmail.com>
2021-04-20 18:23:14 +0900

    usersコントローラーのテストを記述

--- spec/requests/api/v1/users_spec.rb
+++ spec/requests/api/v1/users_spec.rb
@@ -1,7 +1,61 @@
 require 'rails_helper'
 
 RSpec.describe "Api::V1::Users", type: :request do
-  describe "GET /index" do
-    pending "add some examples (or delete) #{__FILE__}"
+  before do
+    @user = FactoryBot.create(:user)
+  end
+
+  describe 'GET /index' do
+    it 'リクエストが成功すること' do
+      get api_v1_users_path
+      expect(response.status).to eq 200
+    end
+    
+    it 'レスポンスに「id」が存在すること' do
+      get api_v1_users_path
+      expect(response.body).to include(@user.id.to_s)
+    end
+
+    it 'レスポンスに「ユーザー名」が存在すること' do
+      get api_v1_users_path
+      expect(response.body).to include(@user.username)
+    end
+
+    it 'レスポンスに「ニックネーム」が存在すること' do
+      get api_v1_users_path
+      expect(response.body).to include(@user.nickname)
+    end
+    
+    it 'レスポンスに「Email」が存在しないこと' do
+      get api_v1_users_path
+      expect(response.body).to include(@user.nickname)
+    end
+  end
+
+  describe 'GET /show' do
+    it 'リクエストが成功すること' do
+      get api_v1_user_path(@user)
+      expect(response.status).to eq 200
+    end
+    
+    it 'レスポンスに「id」が存在すること' do
+      get api_v1_users_path(@user)
+      expect(response.body).to include(@user.id.to_s)
+    end
+
+    it 'レスポンスに「ユーザー名」が存在すること' do
+      get api_v1_users_path(@user)
+      expect(response.body).to include(@user.username)
+    end
+
+    it 'レスポンスに「ニックネーム」が存在すること' do
+      get api_v1_users_path(@user)
+      expect(response.body).to include(@user.nickname)
+    end
+    
+    it 'レスポンスに「Email」が存在しないこと' do
+      get api_v1_users_path(@user)
+      expect(response.body).to include(@user.nickname)
+    end
   end
 end
