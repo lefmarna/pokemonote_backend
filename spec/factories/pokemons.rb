caee9a9d569f925604ac90ce1d9979a151fc2285
lefmarna <panume5ya@gmail.com>
2021-04-22 19:29:52 +0900

    Pokemonモデルのテストに使用するFactoryBotの設定

--- spec/factories/pokemons.rb
+++ spec/factories/pokemons.rb
@@ -1,20 +1,22 @@
 FactoryBot.define do
   factory :pokemon do
-    pokemon_data_id { 1 }
-    nature_data_id { 1 }
-    lv { 1 }
-    hp_iv { 1 }
-    hp_ev { 1 }
-    attack_iv { 1 }
-    attack_ev { 1 }
-    defence_iv { 1 }
-    defence_ev { 1 }
-    sp_attack_iv { 1 }
-    sp_attack_ev { 1 }
-    sp_defence_iv { 1 }
-    sp_defence_ev { 1 }
-    speed_iv { 1 }
-    speed_ev { 1 }
-    description { "MyText" }
+    pokemon_data_id  {Faker::Number.within(range: 1..1038)}
+    nature_data_id   {Faker::Number.within(range: 1..24)}
+    lv               {Faker::Number.within(range: 1..100)}
+    hp_iv            {Faker::Number.within(range: 0..31)}
+    hp_ev            {Faker::Number.within(range: 0..252)}
+    attack_iv        {Faker::Number.within(range: 0..31)}
+    attack_ev        {Faker::Number.within(range: 0..252)}
+    defence_iv       {Faker::Number.within(range: 0..31)}
+    defence_ev       {Faker::Number.within(range: 0..252)}
+    sp_attack_iv     {Faker::Number.within(range: 0..31)}
+    sp_attack_ev     {Faker::Number.within(range: 0..252)}
+    sp_defence_iv    {Faker::Number.within(range: 0..31)}
+    sp_defence_ev    {Faker::Number.within(range: 0..252)}
+    speed_iv         {Faker::Number.within(range: 0..31)}
+    speed_ev         {Faker::Number.within(range: 0..252)}
+    description      {Faker::Lorem.sentence}
+
+    association :user
   end
 end
--- spec/models/pokemon_spec.rb
+++ spec/models/pokemon_spec.rb
@@ -1,5 +1,7 @@
 require 'rails_helper'
 
 RSpec.describe Pokemon, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+  before do
+    @pokemon = FactoryBot.build(:pokemon)
+  end
 end
