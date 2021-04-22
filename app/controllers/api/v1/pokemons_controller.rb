854becbbb58ad4c2034200c5496591339c1660f7
lefmarna <panume5ya@gmail.com>
2021-04-22 18:04:15 +0900

    pokemonsコントローラーのcreateアクションを実装

--- app/controllers/api/v1/pokemons_controller.rb
+++ app/controllers/api/v1/pokemons_controller.rb
@@ -18,7 +18,7 @@ class Api::V1::PokemonsController < ApplicationController
     @pokemon = Pokemon.new(pokemon_params)
 
     if @pokemon.save
-      render json: @pokemon, status: :created, location: @pokemon
+      render json: @pokemon, status: :created
     else
       render json: @pokemon.errors, status: :unprocessable_entity
     end
@@ -46,6 +46,6 @@ class Api::V1::PokemonsController < ApplicationController
 
     # Only allow a trusted parameter "white list" through.
     def pokemon_params
-      params.require(:pokemon).permit(:pokemon_data_id, :nature_data_id, :lv, :hp_iv, :hp_ev, :attack_iv, :attack_ev, :defence_iv, :defence_ev, :sp_attack_iv, :sp_attack_ev, :sp_defence_iv, :sp_defence_ev, :speed_iv, :speed_ev, :description)
+      params.require(:pokemon).permit(:pokemon_data_id, :nature_data_id, :lv, :hp_iv, :hp_ev, :attack_iv, :attack_ev, :defence_iv, :defence_ev, :sp_attack_iv, :sp_attack_ev, :sp_defence_iv, :sp_defence_ev, :speed_iv, :speed_ev, :description).merge(user_id: current_api_v1_user.id)
     end
 end
