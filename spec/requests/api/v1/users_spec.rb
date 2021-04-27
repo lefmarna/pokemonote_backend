daa41d7836f75e97d2c370f68e0726662781a40a
lefmarna <panume5ya@gmail.com>
2021-04-27 23:15:42 +0900

    仕様変更に伴うテストコードの変更

--- spec/requests/api/v1/users_spec.rb
+++ spec/requests/api/v1/users_spec.rb
@@ -34,28 +34,28 @@ RSpec.describe 'Api::V1::Users', type: :request do
 
   describe 'GET /show' do
     it 'リクエストが成功すること' do
-      get api_v1_users_path(@user)
+      get "/api/v1/users/#{@user.username}"
       expect(response.status).to eq 200
     end
 
-    it 'レスポンスに「id」が存在すること' do
-      get api_v1_users_path(@user)
-      expect(response.body).to include(@user.id.to_s)
-    end
-
     it 'レスポンスに「ユーザー名」が存在すること' do
-      get api_v1_users_path(@user)
+      get "/api/v1/users/#{@user.username}"
       expect(response.body).to include(@user.username)
     end
 
     it 'レスポンスに「ニックネーム」が存在すること' do
-      get api_v1_users_path(@user)
+      get "/api/v1/users/#{@user.username}"
       expect(response.body).to include(@user.nickname)
     end
 
+    it 'レスポンスに「id」が存在しないこと' do
+      get "/api/v1/users/#{@user.username}"
+      expect(response.body).not_to include(@user.id.to_s)
+    end
+
     it 'レスポンスに「Email」が存在しないこと' do
-      get api_v1_users_path(@user)
-      expect(response.body).to include(@user.nickname)
+      get "/api/v1/users/#{@user.username}"
+      expect(response.body).not_to include(@user.email)
     end
   end
 end
