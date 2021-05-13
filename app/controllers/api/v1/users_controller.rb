35d681d7540e178939ed31f468025ff5d4014b3c
lefmarna <panume5ya@gmail.com>
2021-05-13 18:05:03 +0900

    ポケモンの順番を投稿順ではなく、更新順に変更

--- app/controllers/api/v1/users_controller.rb
+++ app/controllers/api/v1/users_controller.rb
@@ -7,7 +7,7 @@ class Api::V1::UsersController < ApplicationController
   # GET /users/1
   def show
     @user = User.find_by(username: params[:username])
-    @pokemons = Pokemon.where(user_id: @user.id).includes(:user).order(id: :DESC)
+    @pokemons = Pokemon.where(user_id: @user.id).includes(:user).order(updated_at: :DESC)
     @icon = url_for(@user.icon) if @user.icon.attached?
   end
 end
